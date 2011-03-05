649f849bb507a3831eb1f116994d6244a843ced2
Jeff Smith <smith734@msu.edu>
2011-03-05 15:15:23 -0500

    add incrementor class

--- lib/mongoid_auto_inc.rb
+++ lib/mongoid_auto_inc.rb
@@ -1,3 +1,19 @@
 module MongoidAutoInc
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def auto_increment(name, options={})
+      field name
+      
+      class_eval { before_create { self.send("#{name}=", 
+                                    MongoidAutoInc::Incrementor[self.class.name].inc) } 
+      }
+    end
+  end
+end
+
+module Mongoid
+  module Document
+    include MongoidAutoInc
+  end
 end
---
+++ lib/mongoid_auto_inc/incrementor.rb
@@ -0,0 +1,61 @@
+module MongoidAutoInc
+  class Incrementor
+    class Sequence
+      def initialize(sequence)
+        @sequence = sequence.to_s
+        exists? || create
+      end
+      
+      def exists?
+        collection.find(query).count > 0
+      end
+      
+      def db
+        Mongoid.database
+      end
+      
+      def query
+        { "seq_name" => @sequence }
+      end
+      
+      def collection
+        db['sequences']
+      end
+      
+      def current
+        collection.find_one(query)["number"]
+      end
+      
+      def inc
+        update_number_with("$inc" => { "number" => 1 })
+      end
+      
+      def create(number = 0)
+        collection.insert(query.merge({ "number" => number }))
+      end
+      
+      def set(number)
+        update_number_with("$set" => { "number" => number })
+      end
+      
+      def update_number_with(mongo_func)
+        opts = {
+          "query"  => query,
+          "update" => mongo_func,
+          "new"    => true # return the modified document
+        }
+        collection.find_and_modify(opts)["number"]
+      end
+    end
+    
+    class << self
+      def [](sequence)
+        Sequence.new(sequence)
+      end
+
+      def []=(sequence, number)
+        Sequence.new(sequence).set(number)
+      end
+    end
+  end
+end
